8b76dee89cd30377f8c48ac72d6d31dc6f0147cc
NiedermeyerM <mario.niedermeyer@ait.ac.at>
2024-04-16 09:23:57 +0000

    added parameters and using pipeline from gpf

--- gpf/src/ransac_cylinder.cpp
+++ gpf/src/ransac_cylinder.cpp
@@ -14,6 +14,7 @@
 #include "pcl/sample_consensus/sac_model_cylinder.h"
 #include "pcl/sample_consensus/sac.h"
 #include <pcl/sample_consensus/ransac.h>
+#include <pcl/filters/voxel_grid.h>
 
 /// Type of the input point cloud contents
 typedef pcl::PointXYZRGB PointT;
@@ -82,33 +83,64 @@ public:
 
     // Initialize publisher for marker array
     marker_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(
-        "/cylinder_markers", 10);
+        "/ransac/log_cylinder_markers", 10);
+
+    
+    rclcpp::QoS qos_voxel_grid = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default));
+    pub_voxel_grid_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/ransac/log_pc_voxel_grid", qos_voxel_grid);
 
     // Declare parameters with default values
-    this->declare_parameter<double>("normal_distance_weight", 0.1);
-    this->declare_parameter<int>("max_iterations", 10000);
-    this->declare_parameter<double>("distance_threshold", 0.15);
-    this->declare_parameter<double>("min_radius", 0.01);
-    this->declare_parameter<double>("max_radius", 0.5);
-    this->declare_parameter<double>("normal_search_radius", 0.1);
-    this->declare_parameter<double>("probability", 0.99);
+    this->declare_parameter<bool>("downsampling.enable", false);
+    this->declare_parameter<double>("downsampling.voxel_size", 0.01);
+    this->declare_parameter<double>("normal_est.normal_search_radius", 0.07);
+    this->declare_parameter<int>("sac.min_points_for_cylinder", 50);
+    this->declare_parameter<int>("sac.max_iterations", 1000);
+    this->declare_parameter<double>("sac.distance_threshold", 0.07);
+    this->declare_parameter<double>("sac.min_radius", 0.01);
+    this->declare_parameter<double>("sac.max_radius", 0.5);
+    this->declare_parameter<double>("sac.probability", 0.99);
+    this->declare_parameter<double>("result.inlier_proportion_threshold", 0.5);
+    this->declare_parameter<bool>("result.textural_log_info", true);
   }
 
 private:
   void pointcloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
   {
     // Fetch parameters
-    double normal_distance_weight = this->get_parameter("normal_distance_weight").as_double();
-    int max_iterations = this->get_parameter("max_iterations").as_int();
-    double distance_threshold = this->get_parameter("distance_threshold").as_double();
-    double min_radius = this->get_parameter("min_radius").as_double();
-    double max_radius = this->get_parameter("max_radius").as_double();
-    double normal_search_radius = this->get_parameter("normal_search_radius").as_double();
-    double probability = this->get_parameter("probability").as_double();
-
+    bool downsample_input = this->get_parameter("downsampling.enable").as_bool();
+    double voxel_leaf_size_xyz = this->get_parameter("downsampling.voxel_size").as_double();
+    double normal_search_radius = this->get_parameter("normal_est.normal_search_radius").as_double();
+    int min_points_for_cylinder_ = this->get_parameter("sac.min_points_for_cylinder").as_int();
+    int max_iterations = this->get_parameter("sac.max_iterations").as_int();
+    double distance_threshold = this->get_parameter("sac.distance_threshold").as_double();
+    double min_radius = this->get_parameter("sac.min_radius").as_double();
+    double max_radius = this->get_parameter("sac.max_radius").as_double();
+    double probability = this->get_parameter("sac.probability").as_double();
+    double inlier_proportion_threshold_ = this->get_parameter("result.inlier_proportion_threshold").as_double();
+    bool textural_log_info = this->get_parameter("result.textural_log_info").as_bool();
+    
     // Convert ROS point cloud message to PCL point cloud
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::fromROSMsg(*msg, *pcl_cloud);
+
+    // Downsample input to speed up the computations, if desired
+    if (downsample_input)
+    {
+      RCLCPP_INFO(this->get_logger(), "Size before downsampling: %ld", pcl_cloud->size());
+      pcl::VoxelGrid<PointT> voxel_grid;
+      voxel_grid.setInputCloud(pcl_cloud);
+      voxel_grid.setLeafSize(voxel_leaf_size_xyz,
+                             voxel_leaf_size_xyz,
+                             voxel_leaf_size_xyz);
+      voxel_grid.filter(*pcl_cloud);
+      RCLCPP_INFO(this->get_logger(), "Size after downsampling: %ld", pcl_cloud->size());
+    }
+
+    // Publish voxel grid
+    sensor_msgs::msg::PointCloud2 msg_voxel_grid;
+    pcl::toROSMsg(*pcl_cloud, msg_voxel_grid);
+    msg_voxel_grid.header = msg->header;
+    pub_voxel_grid_->publish(msg_voxel_grid);
 
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr remaining_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     *remaining_cloud = *pcl_cloud;
@@ -117,18 +149,17 @@ private:
     visualization_msgs::msg::MarkerArray markers;
     markers.markers.clear();
 
+    // Create a vector to store the detected cylinders
     std::vector<Cylinder> cylinders;
 
-    // Create nullptr of sample consensus implementation
+    // Create sample consensus obj.
     SampleConsensusEstimator::Ptr sac;
 
-    // Create nullptr of sample consensus models
+    // Create sample consensus models
     pcl::SampleConsensusModelCylinder<PointT, pcl::Normal>::Ptr sac_model_cylinder;
 
     // Create new consideration of the geometric primitives
     Cylinder cylinder;
-
-    // Repeat RANSAC until no more cylinders are found
 
     // Create a map to store the point clouds for each color
     std::map<std::tuple<uint8_t, uint8_t, uint8_t>, pcl::PointCloud<pcl::PointXYZRGB>::Ptr> color_to_cloud_map;
@@ -160,11 +191,8 @@ private:
       pcl::PointCloud<pcl::PointXYZRGB>::Ptr color_cloud = pair.second;
 
       // Only proceed if the color cloud has enough points for a cylinder
-      if (color_cloud->size() > min_points_for_cylinder_)
+      if (color_cloud->size() > (std::size_t)min_points_for_cylinder_)
       {
-        // print color and according pointcloud size
-        // RCLCPP_INFO(this->get_logger(), "Color: %d, %d, %d, Size: %d", std::get<0>(pair.first), std::get<1>(pair.first), std::get<2>(pair.first), color_cloud->size());
-
         // Estimate normals for the segmented point cloud
         pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;
         normal_estimator.setInputCloud(color_cloud);
@@ -186,20 +214,24 @@ private:
 
         if (!sac->computeModel())
         {
-          RCLCPP_INFO(this->get_logger(), "Could not estimate a cylinder model for the given dataset of points: %d", color_cloud->size());
-          // break;
+          RCLCPP_INFO(this->get_logger(), "Could not estimate a cylinder model for the given dataset of points: %ld", color_cloud->size());
+          break;
         }
 
-        // Get the cylinder coefficients
+        // Get the cylinder coefficients and inliers
         sac->getModelCoefficients(cylinder._raw_coefficients);
-
-        // Get the inliers
         sac->getInliers(cylinder.inliers);
-        // RCLCPP_INFO(this->get_logger(), "Inliers: %d", cylinder.inliers.size());
-
         // Get the inlier proportion
         float inlier_proportion = static_cast<float>((float)cylinder.inliers.size()) / static_cast<float>(remaining_cloud->size());
 
+        // Check if the inlier proportion is above a certain threshold
+        if (inlier_proportion < inlier_proportion_threshold_)
+        {
+          RCLCPP_INFO(this->get_logger(), "Inlier proportion is below threshold: %f", inlier_proportion);
+          break;
+        }
+
+        // Init a cylinder
         cylinder.init(marker_id);
         // Extract cylinder limits from the associated pointcloud inliers
         // First create a hyperplane that intersects the cylinder, while being parallel with its flat surfaces
@@ -231,34 +263,11 @@ private:
         // Get centre of the cylinder and define it as the position
         cylinder.model.pose.position = (cylinder.pcl_model.axis.pointAt(min) + cylinder.pcl_model.axis.pointAt(max)) / 2.0;
 
-        // Determne the orientation
+        // Determine the orientation
         cylinder.model.pose.orientation.setFromTwoVectors(Eigen::Vector3d::UnitZ(), cylinder.pcl_model.axis.direction().cast<double>());
 
         // Push with the rest of the cylinders
         cylinders.push_back(cylinder);
-
-        // Check if the inlier proportion is above a certain threshold
-        // if (inlier_proportion < 0.5)
-        // {
-        //     RCLCPP_INFO(this->get_logger(), "Inlier proportion is below threshold.");
-        //     break;
-        // }
-
-        // pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-
-        // Output the cylinder coefficients
-        // if (inliers->indices.size() == 0)
-        // {
-        //   RCLCPP_INFO(this->get_logger(), "Could not estimate a cylinder model for the remaining dataset.");
-        //   break;
-        // }
-
-        // // The seven coefficients of the cylinder are given by a point on its axis, the axis direction, and a radius, as:
-        // // [point_on_axis.x point_on_axis.y point_on_axis.z axis_direction.x axis_direction.y axis_direction.z radius
-        // Eigen::Vector3d axis_direction(cylinder._raw_coefficients[3], cylinder._raw_coefficients[4], cylinder._raw_coefficients[5]);
-
-        // // Create a quaternion from the axis direction
-        // Eigen::Quaterniond orientation = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitZ(), axis_direction);
 
         // Create a marker for the detected cylinder
         visualization_msgs::msg::Marker cylinder_marker;
@@ -278,141 +287,50 @@ private:
         cylinder_marker.scale.x = cylinder.model.radius * 2; // cylinder radius
         cylinder_marker.scale.y = cylinder.model.radius * 2; // cylinder radius
         cylinder_marker.scale.z = cylinder.model.height;     // cylinder height
-        // Get the color from the point cloud
-        pcl::PointXYZRGB point = color_cloud->at(0); // Assuming the color is the same for all points in the cloud
-        cylinder_marker.color.a = point.a / 255.0 / 2.0; // Alpha value
-        cylinder_marker.color.r = point.r / 255.0; // Red value
-        cylinder_marker.color.g = point.g / 255.0; // Green value
-        cylinder_marker.color.b = point.b / 255.0; // Blue value
+        pcl::PointXYZRGB point = color_cloud->at(0);         // using the color coming from the cloud
+        cylinder_marker.color.a = point.a / 255.0 / 2.0;     // half transparent
+        cylinder_marker.color.r = point.r / 255.0;
+        cylinder_marker.color.g = point.g / 255.0;
+        cylinder_marker.color.b = point.b / 255.0;
         markers.markers.push_back(cylinder_marker);
+
+        
+        // Text Marker displaying Diameter, Length and Inlier Proportion of SAC fitted cylinder
+        if(textural_log_info)
+        {  
+          visualization_msgs::msg::Marker text_marker;
+          text_marker.header.frame_id = msg->header.frame_id;
+          text_marker.header.stamp = rclcpp::Time(0);
+          text_marker.id = 1000 - marker_id; // TODO fix this
+          text_marker.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
+          text_marker.action = visualization_msgs::msg::Marker::ADD;
+          text_marker.pose.position.x = cylinder.model.pose.position.x();
+          text_marker.pose.position.y = cylinder.model.pose.position.y();
+          text_marker.pose.position.z = cylinder.model.pose.position.z();
+          text_marker.scale.z = 0.1;
+          text_marker.color.a = 1.0;
+          text_marker.color.r = point.r / 255.0;
+          text_marker.color.g = point.g / 255.0;
+          text_marker.color.b = point.b / 255.0;
+          // Add the text to the marker
+          std::stringstream ss;
+          ss << std::fixed << std::setprecision(2); // 2 digits after the decimal point
+          // Add diameter, length and inlier proportion
+          ss << "Diam.: " << 2 * cylinder.model.radius << "\nLen.: " << cylinder.model.height << "\nInlierProp.: " << inlier_proportion;
+          text_marker.text = ss.str();
+          markers.markers.push_back(text_marker);
+        }
+
       }
     }
 
-    // visualization_msgs::msg::Marker cylinder_marker;
-    // // Cylinders
-    // for (auto &cylinder : cylinders)
-    // {
-    //   // geometric_primitive_msgs::msg::Cylinder c;
-    //   cylinder_marker.id = cylinder.id;
-    //   cylinder_marker.pose.position.x = cylinder.model.pose.position.x();
-    //   cylinder_marker.pose.position.y = cylinder.model.pose.position.y();
-    //   cylinder_marker.pose.position.z = cylinder.model.pose.position.z();
-    //   cylinder_marker.pose.orientation.x = cylinder.model.pose.orientation.x();
-    //   cylinder_marker.pose.orientation.y = cylinder.model.pose.orientation.y();
-    //   cylinder_marker.pose.orientation.z = cylinder.model.pose.orientation.z();
-    //   cylinder_marker.pose.orientation.w = cylinder.model.pose.orientation.w();
-    //   // c.radius = cylinder.model.radius;
-    //   // c.height = cylinder.model.height;
-    //   // msg_primitives.primitives.cylinders.push_back(c);
-    //   cylinder_marker.scale.x = cylinder.model.radius * 2; // cylinder radius
-    //   cylinder_marker.scale.y = cylinder.model.radius * 2; // cylinder radius
-    //   cylinder_marker.scale.z = cylinder.model.height;     // cylinder height
-    //   cylinder_marker.color.a = 1.0;
-    //   cylinder_marker.color.r = 0.0;
-    //   cylinder_marker.color.g = 0.0;
-    //   cylinder_marker.color.b = 1.0;
-    //   markers.markers.push_back(cylinder_marker);
-    // }
     // Publish marker array
     marker_publisher_->publish(markers);
-
-    // while (remaining_cloud->size() > min_points_for_cylinder_)
-    // {
-
-    //   // Estimate normals for the segmented point cloud
-    //   pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;
-    //   normal_estimator.setInputCloud(remaining_cloud);
-    //   pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree_n(new pcl::search::KdTree<pcl::PointXYZRGB>());
-    //   normal_estimator.setSearchMethod(tree_n);
-    //   normal_estimator.setRadiusSearch(normal_search_radius);
-    //   pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>());
-    //   normal_estimator.compute(*cloud_normals);
-
-    //   // save normals to file
-    //   // pcl::io::savePCDFileASCII("normals.pcd", *cloud_normals);
-    //   pcl::io::savePCDFileBinary("normals.pcd", *cloud_normals);
-
-    //   sac_model_cylinder.reset(new pcl::SampleConsensusModelCylinder<PointT, pcl::Normal>(remaining_cloud));
-    //   sac_model_cylinder->setInputNormals(cloud_normals);
-    //   sac_model_cylinder->setRadiusLimits(min_radius, max_radius);
-
-    //   sac.reset(new SampleConsensusEstimator(sac_model_cylinder));
-    //   sac->setDistanceThreshold(distance_threshold);
-    //   sac->setMaxIterations(max_iterations);
-    //   sac->setProbability(0.99);
-
-    //   sac->computeModel();
-    //   // // Perform RANSAC
-    //   // if (!sac->computeModel())
-    //   // {
-    //   //     RCLCPP_INFO(this->get_logger(), "Could not estimate a cylinder model for the given dataset.");
-    //   //     break;
-    //   // }
-
-    //   // Get the cylinder coefficients
-    //   // Eigen::VectorXf coefficients;
-    //   sac->getModelCoefficients(cylinder._raw_coefficients);
-
-    //   // Get the inliers
-    //   // std::vector<int> inliers;
-    //   sac->getInliers(cylinder.inliers);
-
-    //   // Get the inlier proportion
-    //   float inlier_proportion = static_cast<float>((float)cylinder.inliers.size()) / static_cast<float>(remaining_cloud->size());
-
-    //   // Check if the inlier proportion is above a certain threshold
-    //   // if (inlier_proportion < 0.5)
-    //   // {
-    //   //     RCLCPP_INFO(this->get_logger(), "Inlier proportion is below threshold.");
-    //   //     break;
-    //   // }
-
-    //   pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-
-    //   // // Output the cylinder coefficients
-    //   // if (inliers->indices.size() == 0)
-    //   // {
-    //   //     RCLCPP_INFO(this->get_logger(), "Could not estimate a cylinder model for the remaining dataset.");
-    //   //     break;
-    //   // }
-
-    //   // Create a marker for the detected cylinder
-    //   visualization_msgs::msg::Marker marker;
-    //   marker.header.frame_id = msg->header.frame_id;
-    //   marker.header.stamp = msg->header.stamp;
-    //   marker.type = visualization_msgs::msg::Marker::CYLINDER;
-    //   marker.action = visualization_msgs::msg::Marker::ADD;
-    //   marker.pose.position.x = cylinder._raw_coefficients[0];
-    //   marker.pose.position.y = cylinder._raw_coefficients[1];
-    //   marker.pose.position.z = cylinder._raw_coefficients[2];
-    //   marker.pose.orientation.x = 0.0;
-    //   marker.pose.orientation.y = 0.0;
-    //   marker.pose.orientation.z = 0.0;
-    //   marker.pose.orientation.w = 1.0;
-    //   marker.scale.x = cylinder._raw_coefficients[6] * 2; // cylinder radius
-    //   marker.scale.y = cylinder._raw_coefficients[6] * 2; // cylinder radius
-    //   marker.scale.z = cylinder._raw_coefficients[5];     // cylinder height
-    //   marker.color.a = 1.0;
-    //   marker.color.r = 0.0;
-    //   marker.color.g = 0.0;
-    //   marker.color.b = 1.0;
-    //   markers.markers.push_back(marker);
-
-    //   // Extract the inliers of the cylinder and remove them from the remaining cloud
-    //   pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-    //   extract.setInputCloud(remaining_cloud);
-    //   extract.setIndices(inliers);
-    //   extract.setNegative(true);
-    //   extract.filter(*remaining_cloud);
-    // }
-
-    // // Publish marker array
-    // marker_publisher_->publish(markers);
   }
 
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_subscriber_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_;
-  size_t min_points_for_cylinder_ = 50; // Minimum number of points required to detect a cylinder: 100?
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_voxel_grid_;
 };
 
 int main(int argc, char *argv[])
